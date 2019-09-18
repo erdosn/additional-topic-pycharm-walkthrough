6074d2b7f9cea8568e3a3e738646a126be031e4d
Rafael Carrasco <rafael.carrasco@flatironschool.com>
2019-09-18 11:35:34 -0500

    wrapping up study group

--- some_functions.py
+++ some_functions.py
@@ -1,12 +1,34 @@
 import numpy as np
 
 
+def create_dictionary():
+    d = {"a": 1, "b": "this is another value", "c": "here's another value"}
+
+    d["d"] = "and the last value"
+
+    return d
+
+
 def pythagorean_theorem(a, b):
-    return np.sqrt(a**2 + b**2)
+    return np.sqrt(a ** 2 + b ** 2)
 
 
-if __name__=="__main__":
+def is_prime(n):
+    if n == 2:
+        return True
+    top = int(n ** 0.5) + 1
+    for i in range(2, top):
+        if n % i == 0:
+            return False
+    return True
+
+
+if __name__ == "__main__":
     a = 4
     b = 10
     c = pythagorean_theorem(a, b)
     print(c)
+    print(create_dictionary())
+
+    prime_number = 11
+    print(is_prime(11))
--- test_folder/test_some_functions.py
+++ test_folder/test_some_functions.py
@@ -3,8 +3,17 @@ import numpy as np
 import some_functions as sf
 
 
-def test_pythagorean_function_0():
-    a = 0
-    b = 00
-    actual = sf.pythagorean_theorem(a, b)
-    assert expected==actual, "failed first test"
+# def test_pythagorean_function_0():
+#     a = 0
+#     b = 00
+#     actual = sf.pythagorean_theorem(a, b)
+#     assert expected==actual, "failed first test"
+
+
+
+
+def test_is_prime_for_prime():
+    number = 11
+    expected = True
+    actual = sf.is_prime(number)
+    assert actual==expected, f"you failed to identify {number} as prime"
